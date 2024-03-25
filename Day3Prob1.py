a2961981e5d472e69582f18ef59e4ba651837fa3
Noor Ismot Ara <ara.noor.ismot@gmail.com>
2024-03-25 21:41:12 +0100

    Update Day3Prob1.py

--- Day3Prob1.py
+++ Day3Prob1.py
@@ -1,6 +1,5 @@
 #Problem Statement
 """
-Problem statement
 The n-th term of Fibonacci series F(n), where F(n) is a function, is calculated using the following formula -
 
     F(n) = F(n - 1) + F(n - 2), 
@@ -12,20 +11,28 @@ Provided 'n' you have to find out the n-th Fibonacci Number. Handle edges cases 
 
 Example :
 Input: 6
-
 Output: 8
 
 """
 
-userNumber = int(input())
-fibList = list(1)
-if userNumber == 1 :
-    print("1")
-else:
-    for i in range(2, userNumber+1):
-        newNumber = (i- 1) + (i-2)
-        fibList.append(newNumber)
-    print(fibList[userNumber])
+'''
+#Series:
+1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89,144,233...
+'''
+def fibonacci(n):
+    if n <= 0:
+        return "Incorrect Input"
+    elif n == 1 or n == 2:
+        return 1
+    else:
+        fib_sequence = [0, 1, 1]
+        for i in range(3, n + 1):
+            fib_sequence.append(fib_sequence[i - 1] + fib_sequence[i - 2])
+        return fib_sequence[n]
+
+print(fibonacci(int(input())))
+
+
 
 
 
