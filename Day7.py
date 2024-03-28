69e3f1cfa2cd34ddd63a7ac12b8708a3d5c949d7
Noor Ismot Ara <ara.noor.ismot@gmail.com>
2024-03-28 22:21:17 +0100

    Create Day7.py

---
+++ Day7.py
@@ -0,0 +1,28 @@
+#Day 7: Revising basics which I have learnt/practiced last six days
+
+#Input-Output-inputType-typeConversion
+'''
+name = input("Enter your name:")
+age = int(input("Enter your age: "))
+print(type(name))
+print(type(age))
+print("My name is ",name)
+print("My current age: ", age, end=",")
+print("Next year your age will be: ", age+1)
+'''
+
+'''
+#Take multiple Input in one line
+x,y,z = map(int,input().split())
+print(x,y,z)
+print(type(x))
+'''
+
+
+#Take multiple Input in one line
+'''
+myList = list(map(int,input().split()))
+print(myList)
+print(type(myList))
+print(len(myList))
+'''
